4dcd1d1f25047de93713d9bee71905da6470a78e
HassanRad <radhasssan@gmail.com>
2023-02-15 15:24:07 +0330

    add task and base for view

--- toDoList/main.js
+++ toDoList/main.js
@@ -11,17 +11,36 @@ if (list && list.length > 0){
 }
 
 for (let key in arr){
-    console.log(arr[key]);
     let li = document.createElement('li');
+    let div = document.createElement('div');
+    let span = document.createElement('span');
+    let check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.setAttribute('id', arr[key].id);
+
+    let imgDelete = document.createElement('img');
+    imgDelete.setAttribute('src', 'delete.png');
+    imgDelete.setAttribute('id', arr[key].id);
+    let imgEdit = document.createElement('img');
+    imgEdit.setAttribute('src', 'edit.png');
+    imgEdit.setAttribute('id', arr[key].id);
+    
     let txt = document.createTextNode(arr[key].value);
-    li.appendChild(txt)
-    document.getElementById('list').appendChild(li)
+
+    span.appendChild(txt);
+    div.appendChild(check)
+    div.appendChild(span);
+    div.appendChild(imgDelete);
+    div.appendChild(imgEdit);
+    li.appendChild(div);
+    document.getElementById('list').appendChild(li);
+    
 }
+
+
 
 function addToList(){
     let task = document.getElementById('input');
-    let li = document.createElement('li');
-    let txt = document.createTextNode(task.value);
     if (task.value !== ''){
         let toDo = {
             id:(new Date()).getTime(),
@@ -30,9 +49,29 @@ function addToList(){
         }
         arr.push(toDo)
         localStorage.setItem('list', JSON.stringify(arr));
+        let li = document.createElement('li');
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        let check = document.createElement('input');
+        check.setAttribute('type', 'checkbox');
+        check.setAttribute('id', toDo.id);
+
+        let imgDelete = document.createElement('img');
+        imgDelete.setAttribute('src', 'delete.png');
+        imgDelete.setAttribute('id',toDo.id);
+        let imgEdit = document.createElement('img');
+        imgEdit.setAttribute('src', 'edit.png');
+        imgEdit.setAttribute('id', toDo.id);
+        
+        let txt = document.createTextNode(task.value);
         task.value = '';
-        li.appendChild(txt)
-        document.getElementById('list').appendChild(li)
+        span.appendChild(txt);
+        div.appendChild(check)
+        div.appendChild(span);
+        div.appendChild(imgDelete);
+        div.appendChild(imgEdit);
+        li.appendChild(div);
+        document.getElementById('list').appendChild(li);
     }
 }
 
